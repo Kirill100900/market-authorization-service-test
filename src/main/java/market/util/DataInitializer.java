29afa7dddf0ac789515e4934bf93fa3d35135ea9
Ivansivurov <buddamaster@mail.ru>
2022-03-19 12:56:02 +0300

    Добавил создание двух аккаунтов

--- src/main/java/market/util/DataInitializer.java
+++ src/main/java/market/util/DataInitializer.java
@@ -1,15 +1,15 @@
 package market.util;
 
-import market.dao.AccountDao;
 import market.dao.RoleDao;
 import market.model.Account;
 import market.model.Role;
+import market.service.AccountService;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
 @Component
-public record DataInitializer(AccountDao accountDao, RoleDao roleDao) {
+public record DataInitializer(AccountService accountService, RoleDao roleDao) {
 
     @PostConstruct
     private void init() {
@@ -22,11 +22,11 @@ public record DataInitializer(AccountDao accountDao, RoleDao roleDao) {
             }
         }
 
-        if (Boolean.FALSE.equals(accountDao.existAccountByEmail("user@mail.ru"))) {
-            accountDao.saveAccount(new Account("user@mail.ru", "user", roleDao.getRoleByName("ROLE_USER"), false));
+        if (Boolean.FALSE.equals(accountService.existAccountByEmail("user@mail.ru"))) {
+            accountService.saveAccount(new Account("user@mail.ru", "user", roleDao.getRoleByName("ROLE_USER"), false));
         }
-        if (Boolean.FALSE.equals(accountDao.existAccountByEmail("admin@mail.ru"))) {
-            accountDao.saveAccount(new Account("admin@mail.ru", "admin", roleDao.getRoleByName("ROLE_ADMIN"), false));
+        if (Boolean.FALSE.equals(accountService.existAccountByEmail("admin@mail.ru"))) {
+            accountService.saveAccount(new Account("admin@mail.ru", "admin", roleDao.getRoleByName("ROLE_ADMIN"), false));
         }
     }
 }
