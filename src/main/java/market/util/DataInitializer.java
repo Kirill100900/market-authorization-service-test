f34f5860e1c553212b9ed981bbce711e7870c3b5
nikitanesterenko <nikitanesterenkos@gmail.com>
2022-04-15 13:58:23 +0300

    fix

--- src/main/java/market/util/DataInitializer.java
+++ src/main/java/market/util/DataInitializer.java
@@ -8,25 +8,25 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
-@Component
-public record DataInitializer(AccountService accountService, RoleDao roleDao) {
-
-    @PostConstruct
-    private void init() {
-        Roles[] values = Roles.values();
-
-        for (Roles value :
-                values) {
-            if (roleDao.getRoleByName(value.getRoleName()) == null) {
-                roleDao.saveRole(new Role(value.getRoleName()));
-            }
-        }
-
-        if (Boolean.FALSE.equals(accountService.existAccountByEmail("user@mail.ru"))) {
-            accountService.saveAccount(new Account("user@mail.ru", "user", roleDao.getRoleByName("ROLE_USER"), false));
-        }
-        if (Boolean.FALSE.equals(accountService.existAccountByEmail("admin@mail.ru"))) {
-            accountService.saveAccount(new Account("admin@mail.ru", "admin", roleDao.getRoleByName("ROLE_ADMIN"), false));
-        }
-    }
-}
+//@Component
+//public record DataInitializer(AccountService accountService, RoleDao roleDao) {
+//
+//    @PostConstruct
+//    private void init() {
+//        Roles[] values = Roles.values();
+//
+//        for (Roles value :
+//                values) {
+//            if (roleDao.getRoleByName(value.getRoleName()) == null) {
+//                roleDao.saveRole(new Role(value.getRoleName()));
+//            }
+//        }
+//
+//        if (Boolean.FALSE.equals(accountService.existAccountByEmail("user@mail.ru"))) {
+//            accountService.saveAccount(new Account("user@mail.ru", "user", roleDao.getRoleByName("ROLE_USER"), false));
+//        }
+//        if (Boolean.FALSE.equals(accountService.existAccountByEmail("admin@mail.ru"))) {
+//            accountService.saveAccount(new Account("admin@mail.ru", "admin", roleDao.getRoleByName("ROLE_ADMIN"), false));
+//        }
+//    }
+//}
